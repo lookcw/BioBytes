31d6ff29eaa70f10fbd12792315bcc125625f112
lookcw <clook@terpmail.umd.edu>
2018-02-17 16:12:35 -0500

    changing names in dist

--- Calc_Dist.py
+++ Calc_Dist.py
@@ -4,8 +4,8 @@ def distance(lat1, lon1, lat2, lon2):
 	p = 0.017453292519943295 #Pi/180
 	a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
 	return 12742 * asin(sqrt(a)) #2*R*asin...
-par = open("parent_lat_lon.txt",'r')
-child = open("child_lat_lon.txt",'r')
+par = open("par_lat_lon.txt",'r')
+child = open("chi_lat_lon.txt",'r')
 p_loc = par.readline().split(',')
 c_loc = child.readline().split(',')
 
